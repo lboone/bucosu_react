84c8866f28cf45eaff6e6d416fd2b7176d4f2793
Lloyd Boone <lloyd@lambapps.com>
2021-01-08 21:16:45 -0500

    Add Comments Features

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -160,4 +160,86 @@ router.put('/unlike/:id', auth, async (req, res) => {
   } 
 })
 
+
+// @route   PUT api/posts/comment/:id
+// @desc    Comment on a post
+// @access  Private
+router.put('/comment/:id', [auth, [
+  check('text','Text is required').not().isEmpty()
+]], async (req, res) => {
+  const errors = validationResult(req);
+  if(!errors.isEmpty()){
+    return res.status(400).json({errors: errors.array() })
+  }
+
+  try{
+    const user = await User.findById(req.user.id).select('-password')
+    const post = await Post.findById(req.params.id)
+    if(!post){
+      return res.status(404).json({errors: [{msg: 'Post not found'}]})
+    }
+
+    const newComment = {
+      text: req.body.text,
+      name: user.name,
+      avatar: user.avatar,
+      user: req.user.id
+    }
+    post.comments.unshift(newComment)
+    await post.save()
+
+    res.status(200).json(post.comments)
+
+  } catch (err) {
+    console.error(err.message)
+
+    if(err.kind === 'ObjectId'){
+      return res.status(404).json({ errors: [{msg: 'Post not found'}]})   
+    }
+    res.status(500).json({ errors: [{msg: 'Server error'}]})
+  } 
+})
+
+
+// @route   DELETE api/posts/comment/:id/:comment_id
+// @desc    Un Like a post
+// @access  Private
+router.delete('/comment/:id/:comment_id', auth, async (req, res) => {
+  try{
+    const post = await Post.findById(req.params.id)
+    if(!post){
+      return res.status(404).json({errors: [{msg: 'Post not found'}]})
+    }
+
+    // Pull out comment
+    const comment = post.comments.find(comment => comment.id === req.params.comment_id)
+
+    // Make sure comment exists
+    if(!comment){
+      return res.status(404).json({errors: [{msg: 'Comment does not exist'}]})
+    }
+
+    if(comment.user.toString() !== req.user.id){
+      return res.status(401).json({errors: [{msg: 'User not authorized'}]})
+    }
+    
+    // Get the remove index
+    const removeIndex = post.comments.map(comment => comment.user.toString()).indexOf(req.user.id)
+
+    post.comments.splice(removeIndex,1)
+    await post.save()
+
+    res.status(200).json(post.comments)
+
+  } catch (err) {
+    console.error(err.message)
+
+    if(err.kind === 'ObjectId'){
+      return res.status(404).json({ errors: [{msg: 'Post not found'}]})   
+    }
+    res.status(500).json({ errors: [{msg: 'Server error'}]})
+  } 
+})
+
+
 module.exports = router
