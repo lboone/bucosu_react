5d86de5cd34ee692b0fdb54c05ad3b893b990348
Lloyd Boone <lloyd@lambapps.com>
2021-01-14 19:01:55 -0500

    Setup React And Login, Landing, Navbar Routes

---
+++ client/src/components/auth/Login.js
@@ -0,0 +1,59 @@
+import React, { Fragment, useState } from 'react'
+
+const Login = () => {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: ''
+  })
+
+  const { email, password } = formData
+
+  const onChange = e => setFormData({...formData, [e.target.name]: e.target.value })
+  const onSubmit = e => {
+    e.preventDefault();
+    console.log(formData)
+  }
+  return (
+    <Fragment>
+      <div className="alert alert-danger">
+        Invalid credentials
+      </div>
+      <h1 className="large text-primary">Sign In</h1>
+      <p className="lead"><i className="fas fa-user"></i> Sign into Your Account</p>
+      <form className="form" onSubmit={e => onSubmit(e)}>
+        <div className="form-group">
+          <input
+            type="email"
+            placeholder="Email Address"
+            name="email"
+            value={email} 
+            onChange={e => onChange(e)}
+            required 
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={password} 
+            onChange={e => onChange(e)}
+            required 
+          />
+        </div>
+        <input type="submit" className="btn btn-primary" value="Login" />
+      </form>
+    </Fragment>
+  )
+}
+
+export default Login
+
+
+
+
+
+
+
+
+
