b85158ce0848a2691c4c3d40f55f63102dd8584b
Lloyd Boone <lloyd@lambapps.com>
2021-02-02 18:52:31 -0500

    Comments In URLContext

--- client/src/URLContext.js
+++ client/src/URLContext.js
@@ -1,2 +1,11 @@
 import {createContext} from 'react'
-export const URLContext = createContext(null)
+/**
+ * Used to pull in the url parameters.
+ * Sits outside of all the routes and passes down the route parameters as props to all child components.
+ * @param usedIn client/src/routes/index.js
+ * @param howTo  const {action, id, params} = useContext(URLContext)
+ * @param action add / edit / null etc...
+ * @param id     id of document you want to edit / null
+ * @param params all params from passed url / null  
+ */
+export const URLContext = createContext({action:null, id: null, params:{}})
